8ec5b74dcf668ee906411322cab3a10598d40b0d
Paul Solt <PaulSolt@gmail.com>
2020-02-01 20:47:25 -0500

    Starter project

---
+++ HackerRank/HackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Paul Solt on 2/1/20.
+//  Copyright © 2020 Paul Solt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
