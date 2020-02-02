b3b5cc40c34cbbcb8d42f1a4767608f1feda5c39
Paul Solt <PaulSolt@gmail.com>
2020-02-01 21:17:08 -0500

    Added unit tests for the CavityMap Added Struct to wrap logic so we don't get duplicate function names at top level Swift files

--- HackerRank/HackerRank/CavityMap.swift
+++ HackerRank/HackerRank/CavityMap.swift
@@ -8,47 +8,50 @@
 
 import Foundation
 
-func isCavity(grid: [[Character]], x: Int, y: Int) -> Bool {
-    grid[y][x] > grid[y - 1][x] &&
-    grid[y][x] > grid[y][x + 1] &&
-    grid[y][x] > grid[y + 1][x] &&
-    grid[y][x] > grid[y][x - 1]
-}
-
-func to2DGrid(_ grid: [String]) -> [[Character]] {
-    grid.map { Array(String($0)) }
-}
-
-func toOutputGrid(_ grid: [[Character]]) -> [String] {
-    grid.map { String($0) }
-}
-
-// Complete the cavityMap function below.
-func cavityMap(grid: [String]) -> [String] {
-    guard grid.count > 2 else { return grid }
+struct CavityMap {
     
-    let grid = to2DGrid(grid) // read only, X's will break algorithm
-    var output = grid
-    for y in 1 ..< (grid.count - 1) {
-        for x in 1 ..< (grid.count - 1) {
-            if isCavity(grid: grid, x: x, y: y) {
-                output[y][x] = "X"
+    func isCavity(grid: [[Character]], x: Int, y: Int) -> Bool {
+        grid[y][x] > grid[y - 1][x] &&
+        grid[y][x] > grid[y][x + 1] &&
+        grid[y][x] > grid[y + 1][x] &&
+        grid[y][x] > grid[y][x - 1]
+    }
+
+    func to2DGrid(_ grid: [String]) -> [[Character]] {
+        grid.map { Array(String($0)) }
+    }
+
+    func toOutputGrid(_ grid: [[Character]]) -> [String] {
+        grid.map { String($0) }
+    }
+
+    // Complete the cavityMap function below.
+    func cavityMap(grid: [String]) -> [String] {
+        guard grid.count > 2 else { return grid }
+        
+        let grid = to2DGrid(grid) // read only, X's will break algorithm
+        var output = grid
+        for y in 1 ..< (grid.count - 1) {
+            for x in 1 ..< (grid.count - 1) {
+                if isCavity(grid: grid, x: x, y: y) {
+                    output[y][x] = "X"
+                }
             }
         }
+        return toOutputGrid(output)
     }
-    return toOutputGrid(output)
-}
 
-func printGrid(grid: [String]) {
-    let n = grid.count
-    for y in 0 ..< n {
-        for x in 0 ..< n {
-            let row = grid[y]
-            let indicies = Array(row.indices)
-            let digit = row[indicies[x]]
-            print(digit, separator: "", terminator: "")
+    func printGrid(grid: [String]) {
+        let n = grid.count
+        for y in 0 ..< n {
+            for x in 0 ..< n {
+                let row = grid[y]
+                let indicies = Array(row.indices)
+                let digit = row[indicies[x]]
+                print(digit, separator: "", terminator: "")
+            }
+            print()
         }
-        print()
     }
 }
 
--- HackerRank/HackerRankTests/CavityMapTests.swift
+++ HackerRank/HackerRankTests/CavityMapTests.swift
@@ -7,13 +7,55 @@
 //
 
 import XCTest
-//@testable HackerRank
-//import HackerRank
 
 class CavityMapTests: XCTestCase {
-
+    
+    var cavity = CavityMap()
+    
     func test3x3CavityMap() {
+        let input =
+            ["1112",
+             "1912",
+             "1892",
+             "1234"]
+        let expected =
+            ["1112",
+             "1X12",
+             "18X2",
+             "1234"]
         
+        let actual = cavity.cavityMap(grid: input)
+        
+        XCTAssertEqual(expected, actual)
     }
+    
+    func test9x9CavityMap() {
+        let input =
+            ["179443854",
+            "961621369",
+            "164139922",
+            "968633951",
+            "812882578",
+            "257829163",
+            "812438597",
+            "176656233",
+            "485773814"]
 
+        let expected =
+            ["179443854",
+            "961X21369",
+            "164139922",
+            "96X633951",
+            "812882578",
+            "25782X163",
+            "8124385X7",
+            "176656233",
+            "485773814"]
+        
+        let actual = cavity.cavityMap(grid: input)
+        cavity.printGrid(grid: actual)
+        
+        XCTAssertEqual(expected, actual)
+    }
+    
 }
